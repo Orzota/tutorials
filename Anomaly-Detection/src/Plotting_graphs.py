414ca849e198ea4bcb1b3feb7f67dc37bf143811
Sai Teja <30465745+sai36@users.noreply.github.com>
2018-05-02 12:20:44 +0530

    Update Plotting_graphs.py

--- Anomaly-Detection/src/Plotting_graphs.py
+++ Anomaly-Detection/src/Plotting_graphs.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from datetime import datetime
 
+data = pd.read_csv('/path/to/csvfile.csv')
+
+#For converting the 'data' into the format required for plotting
+dat = [None] * len(data['value'])
+time = data['timestamp'].astype(basestring)
+for i in range(0,len(time)):
+    dat[i] = datetime.strptime(time[i], '%Y-%m-%d %H:%M:%S')
+
+
+#For plotting the time-series data
+
+#Can select the size of the figure
+plt.figure(figsize = (10,6))
+plt.plot(dat,data['values'])
+plt.show()
